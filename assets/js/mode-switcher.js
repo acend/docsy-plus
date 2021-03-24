370b2dc6231f251487e68e75bcfba48e2a403bc9
Mathis Hofer <hofer@puzzle.ch>
2021-03-24 12:13:12 +0100

    Refactor mode switch & add label

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -1,40 +1,58 @@
 {{ if .Site.Params.ModeSwitcher }}
-function setMode(trainingMode){
-    document.querySelectorAll("details").forEach(details => {
-        // only open detail elements with data-mode="expertmode" attributes
-        if(details.getAttribute("data-mode") === "normalexpertmode"){
-            if(trainingMode === "expert"){
-                details.removeAttribute("open");
-            }else{
-                details.setAttribute("open", "true");
-            }
+
+window.addEventListener("load", () => {
+  const TRAINING_MODE_KEY = "trainingMode";
+  const TRAINING_MODE_NORMAL = "normal";
+  const TRAINING_MODE_EXPERT = "expert";
+
+  // Add expert mode switch to navigation
+  const template = `
+      <li class="mode-switch-item nav-item mr-5 mb-2 mb-lg-0 d-flex align-items-center">
+        <label for="mode-switch-input" class="mb-0 mr-2">Expert Mode:</label>
+        <label class="switch mb-0">
+          <input type="checkbox" id="mode-switch-input">
+          <span class="slider"></span>
+        </label>
+      </li>
+    `;
+  const nav = document.getElementById("main_navbar");
+  nav.querySelector("ul").insertAdjacentHTML("afterbegin", template);
+
+  const input = document.getElementById("mode-switch-input");
+  const initialMode = readMode();
+  input.addEventListener("change", onModeChange);
+  input.checked = initialMode === TRAINING_MODE_EXPERT;
+  updateDetailsOpenState(initialMode);
+
+  function readMode() {
+    return localStorage.getItem(TRAINING_MODE_KEY) || TRAINING_MODE_NORMAL;
+  }
+
+  function saveMode(mode) {
+    localStorage.setItem(TRAINING_MODE_KEY, mode || TRAINING_MODE_NORMAL);
+  }
+
+  function onModeChange(event) {
+    const mode = event.currentTarget.checked
+      ? TRAINING_MODE_EXPERT
+      : TRAINING_MODE_NORMAL;
+    saveMode(mode);
+    updateDetailsOpenState(mode);
+  }
+
+  function updateDetailsOpenState(mode) {
+    document.querySelectorAll("details").forEach((details) => {
+      // Only open detail elements with data-mode="normalexpertmode" attributes
+      if (details.dataset.mode === "normalexpertmode") {
+        if (mode === TRAINING_MODE_EXPERT) {
+          details.removeAttribute("open");
+        } else {
+          details.setAttribute("open", "true");
         }
+      }
     });
-}
-
-// add expertmodeswitcher to navigation
-var str = '<li class="nav-item mr-4 mb-2 mb-lg-0"><label class="switch"><input type="checkbox" class="modeswitcher"><span class="slider"></span></label></li>',
-  div = document.getElementById('main_navbar');
-div.querySelector("ul").insertAdjacentHTML("afterbegin", str);
-
-var trainingMode = (localStorage.getItem('trainingMode') === undefined) ? "normal" : localStorage.getItem('trainingMode');
-localStorage.setItem('trainingMode', trainingMode);
-document.querySelectorAll(".modeswitcher").forEach(checkbox => {
-    checkbox.checked = (trainingMode === "expert");
+  }
 });
 
-window.addEventListener('load', (event) => {
-    document.querySelectorAll(".modeswitcher").forEach(checkbox => {
-        checkbox.addEventListener('change', (event) => {
-            if (event.currentTarget.checked) {
-                localStorage.setItem('trainingMode', "expert");
-                setMode("expert");
-            } else {
-                localStorage.setItem('trainingMode', "normal");
-                setMode("normal");
-            }
-        });
-    });
-    setMode(trainingMode);
-});
+
 {{end}}
