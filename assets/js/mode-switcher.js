72efd7ab592892b8cff604c9cb6b58198ea72cae
jyniybinc <1837725+jyniybinc@users.noreply.github.com>
2022-03-08 15:41:06 +0100

    Rewrite hook partials

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -1,5 +1,3 @@
-{{ if .Site.Params.ModeSwitcher }}
-
 window.addEventListener("load", () => {
   const TRAINING_MODE_KEY = "trainingMode";
   const TRAINING_MODE_NORMAL = "normal";
@@ -55,6 +53,3 @@ window.addEventListener("load", () => {
     });
   }
 });
-
-
-{{end}}
